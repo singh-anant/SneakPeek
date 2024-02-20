bc3b4112b010b01d99564fbc1bc7b2c1d98cdbf7
Anant Singh <97743499+singh-anant@users.noreply.github.com>
2024-02-20 09:46:35 +0530

    integrate payment using gpay api

--- FrontEnd/src/pages/CartPage.jsx
+++ FrontEnd/src/pages/CartPage.jsx
@@ -3,6 +3,7 @@ import ProductInCart from '../components/ProductInCart'
 import useCartStore from '../app/cartStore';
 import useAmountStore from '../app/amountStore';
 import EmptyCartPage from './EmptyCartPage';
+import GooglePayButton from '@google-pay/button-react'
 
 
 const CartPage = () => {
@@ -31,7 +32,45 @@ const CartPage = () => {
       </div>
       
       <div className="proced_payment">
-        <button >Checkout</button>
+        <GooglePayButton
+        buttonColor='white'
+        buttonType='checkout'
+  environment="TEST"
+  paymentRequest={{
+    apiVersion: 2,
+    apiVersionMinor: 0,
+    allowedPaymentMethods: [
+      {
+        type: 'CARD',
+        parameters: {
+          allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+          allowedCardNetworks: ['MASTERCARD', 'VISA'],
+        },
+        tokenizationSpecification: {
+          type: 'PAYMENT_GATEWAY',
+          parameters: {
+            gateway: 'example',
+            gatewayMerchantId: 'exampleGatewayMerchantId',
+          },
+        },
+      },
+    ],
+    merchantInfo: {
+      merchantId: '12345678901234567890',
+      merchantName: 'Demo Merchant',
+    },
+    transactionInfo: {
+      totalPriceStatus: 'FINAL',
+      totalPriceLabel: 'Total',
+      totalPrice: amount,
+      currencyCode: 'USD',
+      countryCode: 'US',
+    },
+  }}
+  onLoadPaymentData={paymentRequest => {
+    console.log('load payment data', paymentRequest);
+  }}
+/>
       </div>
     </div>
   </div>
